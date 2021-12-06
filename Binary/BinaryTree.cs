44f11fed0b8a2f5d06917516d68071e59c6196a8
EmelyanovAndreyNSK <50708580+EmelyanovAndreyNSK@users.noreply.github.com>
2021-12-06 10:17:06 +0700

    Add files via upload

---
+++ Binary/BinaryTree.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Generics.BinaryTrees
+{
+    static class BinaryTree
+    {
+        public static BinaryTree<T> Create<T>(params T[] nodeValues) where T : IComparable
+        {
+            var binaryTree = new BinaryTree<T>();
+            foreach (var newValue in nodeValues)
+            {
+                binaryTree.Add(newValue);
+            }
+            return binaryTree;
+        }
+    }
+
+    class BinaryTree<T> : IEnumerable<T> where T : IComparable
+    { 
+        public BinaryTree<T> Left { get; private set; }
+        public BinaryTree<T> Right { get; private set; }
+        public T Value { get; private set; }
+        private bool IsEmpty { get; set; }
+
+        public BinaryTree()
+        {
+            Left = null;
+            Right = null;
+            Value = default;
+            IsEmpty = false;
+        }
+
+        public BinaryTree(T value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+            IsEmpty = true;
+        }
+
+        public void Add(T newValue)
+        {
+            if (!IsEmpty)
+            {
+                Value = newValue;
+                IsEmpty = true;
+                return;
+            }
+
+            IsEmpty = true;
+
+            if (Value.CompareTo(newValue) >= 0)
+            {
+                if (Left != null)
+                {
+                    Left.Add(newValue);
+                }
+                else
+                {
+                    Left = new BinaryTree<T>(newValue);
+                }
+            }
+            else
+            {
+                if (Right != null)
+                {
+                    Right.Add(newValue);
+                }
+                else
+                {
+                    Right = new BinaryTree<T>(newValue);
+                }
+            }
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return GetEnumerable().GetEnumerator();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return GetEnumerable().GetEnumerator();
+        }
+
+        public IEnumerable<T> GetEnumerable()
+        {
+            if (!IsEmpty)
+            {
+                yield break;
+            }
+
+            if (Left != null)
+            {
+                foreach (var nodeValue in Left.GetEnumerable())
+                {
+                    yield return nodeValue;
+                }
+            }
+
+            yield return Value;
+
+            if (Right != null)
+            {
+                foreach (var nodeValue in Right.GetEnumerable())
+                {
+                    yield return nodeValue;
+                }
+            }
+        }
+    }
+}
---
+++ FluentApi/DotGraphBuilder.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FluentApi.Graph
+{
+    public enum NodeShape
+    {
+        Box,
+        Ellipse
+    }
+
+    public class DotGraphBuilder
+    {
+        protected Graph graph;
+        public static DotGraphBuilder UndirectedGraph(string graphName)
+        {
+            var newGraph = new DotGraphBuilder(graphName, false);
+            return newGraph;
+        }
+
+        public static DotGraphBuilder DirectedGraph(string graphName)
+        {
+            var newGraph = new DotGraphBuilder(graphName, true);
+            return newGraph;
+        }
+        
+        public DotGraphBuilder(string graphName, bool isDirected)
+        {
+            this.graph = new Graph(graphName, isDirected, false);
+        }
+
+        public DotGraphBuilder(Graph inputGraph)
+        {
+            this.graph = inputGraph;
+        }
+
+        public string Build() => graph.ToDotFormat();
+
+        public Node AddNode(string nodeContent)
+        {
+            graph.AddNode(nodeContent);
+
+            var newNode = new Node(graph, nodeContent);
+            return newNode;
+        }
+
+        public DotEdge AddEdge(string source, string destination)
+        {
+            graph.AddEdge(source, destination);
+
+            var newDotEdge = new DotEdge(graph, source, destination);
+            return newDotEdge;
+        }
+    }
+
+
+    public class Node : DotGraphBuilder
+    {
+        private string nodeContent;
+        public Node(Graph graph, string inputContent) : base(graph)
+        {
+            nodeContent = inputContent;
+        }
+
+        public DotGraphBuilder With(Action<NodeBuilder> action)
+        {
+            foreach (var node in graph.Nodes)
+                if (node.Name == nodeContent)
+                {
+                    action(new NodeBuilder(node));
+                    break;
+                }
+            return this;
+        }
+    }
+
+
+    public class DotEdge : DotGraphBuilder
+    {
+        private GraphEdge edge;
+        public DotEdge(Graph graph, string fromNode, string toNode) : base(graph)
+        {
+            edge = new GraphEdge(fromNode, toNode, graph.Directed);
+            //edge.SourceNode = fromNode;
+            //edge.DestinationNode = toNode;
+        }
+
+        public DotGraphBuilder With(Action<EdgeBuilder> someAction)
+        {
+            foreach (var graphEdge in graph.Edges)
+                if (graphEdge.SourceNode == edge.SourceNode &&
+                    graphEdge.DestinationNode == edge.DestinationNode)
+                {
+                    someAction(new EdgeBuilder(graphEdge));
+                    break;
+                }
+            return this;
+        }
+    }
+
+    public class NodeBuilder : Properties<NodeBuilder>
+    {
+        public NodeBuilder(GraphNode node)
+        {
+            properties = node.Attributes;
+        }
+
+        public NodeBuilder Shape(NodeShape form)
+        {
+            properties["shape"] = form.ToString().ToLower();
+            return this;
+        }
+    }
+
+    public class EdgeBuilder : Properties<EdgeBuilder>
+    {
+        public EdgeBuilder(GraphEdge edge)
+        {
+            properties = edge.Attributes;
+        }
+        public EdgeBuilder Weight(double weight)
+        {
+            properties["weight"] = weight.ToString();
+            return this;
+        }
+    }
+
+    public class Properties<T> where T : class
+    {
+        protected Dictionary<string, string> properties =
+            new Dictionary<string, string>();
+        public T Color(string newColor)
+        {
+            properties["color"] = newColor;
+            return this as T;
+        }
+
+        public T Label(string newLabel)
+        {
+            properties["label"] = newLabel;
+            return this as T;
+        }
+
+        public T FontSize(int newfSize)
+        {
+            properties["fontsize"] = newfSize.ToString();
+            return this as T;
+        }
+    }
+}
